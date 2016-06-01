23952e15ab269954b33e0039ca6f5af9ab9a7ff1
Evol Johnson <ejohnson@ngeosone.com>
2016-06-01 14:45:59 -0400

    "Generated Wed Jun 01 14:45:55 EDT 2016"

---
+++ sql/createTablesMySQL.sql
@@ -0,0 +1,33 @@
+USE TodoList;
+
+ CREATE TABLE `Status` (
+    `id` bigint(20) NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255),
+    PRIMARY KEY (`id`)
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+ 
+ CREATE TABLE `Task` (
+    `completed` BIT,
+    `id` bigint(20) NOT NULL AUTO_INCREMENT,
+    `order` INT(10),
+    `title` VARCHAR(255),
+    `todo_id` bigint(20) NOT NULL,
+    KEY `FK1464806711352` (`todo_id`),
+    CONSTRAINT `FK1464806711352` FOREIGN KEY (`todo_id`) REFERENCES `TodoItem` (`id`),
+    PRIMARY KEY (`id`)
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+ 
+ CREATE TABLE `TodoItem` (
+    `dueDate` Date,
+    `id` bigint(20) NOT NULL AUTO_INCREMENT,
+    `notes` VARCHAR(255),
+    `status_id` bigint(20) NOT NULL,
+    `tasks_id` bigint(20) NOT NULL,
+    `title` VARCHAR(255),
+    KEY `FK1464806711389` (`status_id`),
+    CONSTRAINT `FK1464806711389` FOREIGN KEY (`status_id`) REFERENCES `Status` (`id`),
+    KEY `FK1464806711389` (`tasks_id`),
+    CONSTRAINT `FK1464806711389` FOREIGN KEY (`tasks_id`) REFERENCES `Task` (`id`),
+    PRIMARY KEY (`id`)
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+ 
